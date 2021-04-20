ee135a3fd65af2e6a8cd24e4689359f13946bf78
Tobias <tobias.boehm@iteratec.com>
2021-04-20 18:36:33 +0200

    Implement colored background tiles

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const NUM_VERTICAL_TILES: number = 39;
+export const NUM_HORIZONTAL_TILES: number = 39;
+export const TILE_SIZE: number = 100;
+export const TIME_BETWEEN_BEATS_IN_SECONDS: number = 500;
+export const WHITE: number = 0xFFFFFF;
+export const ORANGE: number = 0xFFA500;
+export const PURPLE: number = 0x9932CC;
--- src/gameScene.ts
+++ src/gameScene.ts
@@ -1,3 +1,6 @@
+import { NUM_HORIZONTAL_TILES, NUM_VERTICAL_TILES, ORANGE, PURPLE, TILE_SIZE, TIME_BETWEEN_BEATS_IN_SECONDS, WHITE } from "./constants";
+
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
     visible: false,
@@ -5,34 +8,76 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
-    private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
+    private tiles: Phaser.GameObjects.Rectangle[][] = [];
+    private triggerTimer: Phaser.Time.TimerEvent;
+    private tilesAreOrange: boolean = true;
 
     constructor() {
         super(sceneConfig);
     }
 
     public create() {
-        this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFFFF) as any;
-        this.physics.add.existing(this.square);
+        this.createBackground();
     }
 
     public update() {
         const cursorKeys = this.input.keyboard.createCursorKeys();
 
-        if (cursorKeys.up.isDown) {
-            this.square.body.setVelocityY(-500);
-        } else if (cursorKeys.down.isDown) {
-            this.square.body.setVelocityY(500);
-        } else {
-            this.square.body.setVelocityY(0);
+        // if (cursorKeys.up.isDown) {
+        //     this.square.body.setVelocityY(-500);
+        // } else if (cursorKeys.down.isDown) {
+        //     this.square.body.setVelocityY(500);
+        // } else {
+        //     this.square.body.setVelocityY(0);
+        // }
+
+        // if (cursorKeys.right.isDown) {
+        //     this.square.body.setVelocityX(500);
+        // } else if (cursorKeys.left.isDown) {
+        //     this.square.body.setVelocityX(-500);
+        // } else {
+        //     this.square.body.setVelocityX(0);
+        // }
+    }
+
+    private createBackground() {
+        let color: number = WHITE;
+        for (let i = 0; i < NUM_HORIZONTAL_TILES; i++) {
+            this.tiles[i] = [];
+            for (let m = 0; m < NUM_VERTICAL_TILES; m++) {
+                this.tiles[i][m] = this.add.rectangle(TILE_SIZE / 2 + i * TILE_SIZE, TILE_SIZE / 2 + m * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
+                if (color == WHITE) {
+                    color = ORANGE;
+                } else {
+                    color = WHITE;
+                }
+            }
+
         }
 
-        if (cursorKeys.right.isDown) {
-            this.square.body.setVelocityX(500);
-        } else if (cursorKeys.left.isDown) {
-            this.square.body.setVelocityX(-500);
-        } else {
-            this.square.body.setVelocityX(0);
+        this.triggerTimer = this.time.addEvent({
+            callback: this.timerEvent,
+            callbackScope: this,
+            delay: TIME_BETWEEN_BEATS_IN_SECONDS,
+            loop: true
+        });
+    }
+
+    private timerEvent() {
+        for (let i = 0; i < NUM_HORIZONTAL_TILES; i++) {
+            for (let m = 0; m < NUM_VERTICAL_TILES; m++) {
+                if (this.tiles[i][m].fillColor == WHITE) {
+                    if (this.tilesAreOrange) {
+                        this.tiles[i][m].fillColor = PURPLE;
+                    } else {
+                        this.tiles[i][m].fillColor = ORANGE;
+                    }
+                } else {
+                    this.tiles[i][m].fillColor = WHITE;
+                }
+            }
+
         }
+        this.tilesAreOrange = !this.tilesAreOrange;
     }
 }
